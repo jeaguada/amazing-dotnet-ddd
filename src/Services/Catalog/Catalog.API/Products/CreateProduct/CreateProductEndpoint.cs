91bb0fdd2ab4722130d331fa42bb7e51371c6595
eaguada <69743311+jeaguada@users.noreply.github.com>
2024-07-30 07:40:45 -0300

    Before di

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, string Description, decimal Price, string PictureFileName, List<string> Categories) : ICommand<CreateProductResult>;
+    public  record CreateProductResponse(Guid Id);
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest req, ISender sender) =>
+            {
+                var command = req.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.Id}", response);
+            })
+                .WithName("CreateProduct")
+                .Produces<CreateProductResponse>(201)
+                .ProducesProblem(400)
+                .WithSummary("Creates a new product")
+                .WithDescription("Creates a new product in the catalog");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, decimal Price, string PictureFileName, List<string> Categories) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Description = request.Description,
+                Price = request.Price,
+                PictureFileName = request.PictureFileName,
+                Categories = request.Categories
+            };
+
+            return new Task<CreateProductResult>(() => new CreateProductResult(Guid.NewGuid()));
+        }
+    }
+}
